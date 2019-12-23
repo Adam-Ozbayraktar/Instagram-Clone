84ee288033de80a37eb9cd0d100bf34960530100
AdamOz <adamozbayraktar@live.com>
2019-12-23 09:07:45 +0200

    Added stories

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -13,127 +13,193 @@ class Post extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var randomNumber = Random();
-    return ListView.builder(
-      itemBuilder: (ctx, index) {
-        return Card(
-          child: Column(
-            children: <Widget>[
-              // SizedBox(
-              //   height: 20,
-              // ),
-              Container(
-                // color: Colors.black,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  // crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    // child: Image.asset('assets/images/profile-picture.jpg'),
-                    GestureDetector(
-                      child: Padding(
-                        padding: EdgeInsets.all(0),
-                        child: CircleAvatar(
-                          backgroundImage:
-                              AssetImage('assets/images/profile/$index.jpg'),
-                          radius: 20,
-                        ),
+    return Column(
+      children: <Widget>[
+        Container(
+          // margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          height: 80,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 9,
+            itemBuilder: (ctx, index) {
+              return Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    width: 60,
+                    height: 60,
+                    child: index == 0
+                        ? Stack(
+                            children: <Widget>[
+                              CircleAvatar(
+                                backgroundImage: AssetImage(
+                                    'assets/images/profile/$index.jpg'),
+                                radius: 35,
+                              ),
+                              Positioned(
+                                top: 41,
+                                left: 40,
+                                child: Stack(children: <Widget>[
+                                  Icon(
+                                    MdiIcons.circle,
+                                    size: 19,
+                                    color: Colors.white,
+                                  ),
+                                  Icon(
+                                    Icons.add_circle,
+                                    size: 20,
+                                    color: Colors.blue,
+                                  ),
+                                ]),
+                              ),
+                            ],
+                          )
+                        : CircleAvatar(
+                            backgroundImage:
+                                AssetImage('assets/images/profile/$index.jpg'),
+                            radius: 35,
+                          ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                    child: Text(
+                      index == 0 ? 'Your story' : 'genericuser',
+                      style: TextStyle(
+                        fontSize: 11,
                       ),
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (_) {
-                          return DetailScreen(index);
-                        }));
-                      },
                     ),
-
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                child: Column(
+                  children: <Widget>[
+                    // SizedBox(
+                    //   height: 20,
+                    // ),
                     Container(
-                      width: 270,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                      // color: Colors.black,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        // crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: <Widget>[
-                          Text(
-                            'genericuser',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 17,
+                          // child: Image.asset('assets/images/profile-picture.jpg'),
+                          GestureDetector(
+                            child: Padding(
+                              padding: EdgeInsets.all(0),
+                              child: CircleAvatar(
+                                backgroundImage: AssetImage(
+                                    'assets/images/profile/$index.jpg'),
+                                radius: 20,
+                              ),
+                            ),
+                            onTap: () {
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (_) {
+                                return DetailScreen(index);
+                              }));
+                            },
+                          ),
+
+                          Container(
+                            width: 270,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: <Widget>[
+                                Text(
+                                  'genericuser',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                                Text(
+                                  'Los Angeles, California',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                          Text(
-                            'Los Angeles, California',
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.grey,
-                            ),
-                          ),
+
+                          // SizedBox(
+                          //   width: 190,
+                          // ),
+                          Container(
+                              // child: IconButton(
+                              child: PopupMenuButton(
+                            itemBuilder: (context) => [
+                              PopupMenuItem(
+                                child: Text('Report'),
+                              ),
+                              PopupMenuItem(
+                                child: Text('Share'),
+                              ),
+                              PopupMenuItem(
+                                child: Text('Unfollow'),
+                              ),
+                            ],
+                          )
+                              // iconSize: 30,
+                              // icon: Icon(Icons.more_vert),
+                              // icon: Icon(MdiIcons.heart, color: Colors.red),
+                              // icon: Icon(MdiIcons.heartOutline),
+                              // onPressed: () {},
+                              // ),
+                              ),
                         ],
                       ),
                     ),
-
-                    // SizedBox(
-                    //   width: 190,
-                    // ),
+                    // PostImage(index),
                     Container(
-                        // child: IconButton(
-                        child: PopupMenuButton(
-                      itemBuilder: (context) => [
-                        PopupMenuItem(
-                          child: Text('Report'),
+                      height: 400,
+                      width: MediaQuery.of(context).size.width,
+                      child: ClipRect(
+                        // child: Image.asset('assets/images/posts/$index.jpg'),
+                        child: PhotoView(
+                          imageProvider: AssetImage(
+                            'assets/images/posts/$index.jpg',
+                          ),
+                          minScale: PhotoViewComputedScale.contained * 1,
+                          maxScale: PhotoViewComputedScale.covered * 1.8,
+                          initialScale: PhotoViewComputedScale.covered * 1,
                         ),
-                        PopupMenuItem(
-                          child: Text('Share'),
-                        ),
-                        PopupMenuItem(
-                          child: Text('Unfollow'),
-                        ),
-                      ],
-                    )
-                        // iconSize: 30,
-                        // icon: Icon(Icons.more_vert),
-                        // icon: Icon(MdiIcons.heart, color: Colors.red),
-                        // icon: Icon(MdiIcons.heartOutline),
-                        // onPressed: () {},
-                        // ),
-                        ),
+                      ),
+                    ),
+                    GradientIconButtons(
+                      reaction_1: randomNumber.nextInt(100),
+                      reaction_2: randomNumber.nextInt(100),
+                    ),
+                    // AltFooter(
+                    //   hearts: randomNumber.nextInt(100),
+                    //   thumbsUp: randomNumber.nextInt(100),
+                    //   displeased: randomNumber.nextInt(100),
+                    //   angry: randomNumber.nextInt(100),
+                    //   sad: randomNumber.nextInt(100),
+                    // ),
+                    // Footer(
+                    //   hearts: randomNumber.nextInt(100),
+                    //   thumbsUp: randomNumber.nextInt(100),
+                    //   displeased: randomNumber.nextInt(100),
+                    //   angry: randomNumber.nextInt(100),
+                    //   sad: randomNumber.nextInt(100),
+                    // ),
                   ],
                 ),
-              ),
-              // PostImage(index),
-              Container(
-                height: 400,
-                width: MediaQuery.of(context).size.width,
-                child: ClipRect(
-                  // child: Image.asset('assets/images/posts/$index.jpg'),
-                  child: PhotoView(
-                    imageProvider: AssetImage(
-                      'assets/images/posts/$index.jpg',
-                    ),
-                    minScale: PhotoViewComputedScale.contained * 1,
-                    maxScale: PhotoViewComputedScale.covered * 1.8,
-                    initialScale: PhotoViewComputedScale.covered * 1,
-                  ),
-                ),
-              ),
-              GradientIconButtons(
-                reaction_1: randomNumber.nextInt(100),
-                reaction_2: randomNumber.nextInt(100),
-              ),
-              // AltFooter(
-              //   hearts: randomNumber.nextInt(100),
-              //   thumbsUp: randomNumber.nextInt(100),
-              //   displeased: randomNumber.nextInt(100),
-              //   angry: randomNumber.nextInt(100),
-              //   sad: randomNumber.nextInt(100),
-              // ),
-              // Footer(
-              //   hearts: randomNumber.nextInt(100),
-              //   thumbsUp: randomNumber.nextInt(100),
-              //   displeased: randomNumber.nextInt(100),
-              //   angry: randomNumber.nextInt(100),
-              //   sad: randomNumber.nextInt(100),
-              // ),
-            ],
+              );
+            },
+            itemCount: 9,
           ),
-        );
-      },
-      itemCount: 9,
+        ),
+      ],
     );
   }
 }
